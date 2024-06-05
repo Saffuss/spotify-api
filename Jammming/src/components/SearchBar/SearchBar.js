4b8ccb8fac2d76cb80e01439750b9dcdd8985abc
saffuss <smithfuss@gmail.com>
2024-06-05 17:39:18 -0600

    Coded SearchBar.js

--- Jammming/src/components/SearchBar/SearchBar.js
+++ Jammming/src/components/SearchBar/SearchBar.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
 
 function SearchBar() {
+    const [query, setQuery] = useState('');
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if (query.trim() === '') return;
+
+        const url = `${query}`;
+
+        fetch(url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            })
+    }
+
     return (
         <>
-            <p>SearchBar</p>
+            <form onSubmit={handleSubmit} className='search-bar-container'>
+                <input
+                    type='text'
+                    placeholder='Thingies...'
+                    aria-label='Search'
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                <button type='submit'>Search</button>
+            </form>
         </>
     );
 }
