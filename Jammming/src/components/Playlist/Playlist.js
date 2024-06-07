2565767c600d6cbe1c7f157676133653d0e14122
saffuss <smithfuss@gmail.com>
2024-06-07 12:12:34 -0600

    Add PlaylistName.js and implement it in Playlist.js

--- Jammming/src/components/Playlist/Playlist.js
+++ Jammming/src/components/Playlist/Playlist.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Playlist.css';
 import Tracklist from '../Tracklist/Tracklist';
+import PlaylistName from '../../PlaylistName';
 
 function Playlist({ tracklistItems, setTracklistItems }) {
+    const [name, setName] = useState('')
+
     return (
         <div className='playlist'>
-            <h2>Playlist</h2>
+            <PlaylistName name={name} setName={setName} />
             <Tracklist tracklistItems={tracklistItems} setTracklistItems={setTracklistItems} />
             <button type='button'>Save to Spotify</button>
         </div>
